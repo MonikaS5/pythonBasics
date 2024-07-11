11c86c6cc5f831d33513e344354e84ee160bec45
MonikaS5 <monika.bhagade@gmail.com>
2024-07-11 13:21:25 +0100

    first commit_variables

---
+++ app.py
@@ -0,0 +1,22 @@
+num = 10
+print("Hello World!")
+print(num)
+first_name = "John"
+print(first_name)
+is_true = True
+print(is_true)
+
+
+name = input("What is your name? ")
+
+print("Hello " + name)
+
+birth_year = input("Enter your birth year : ")
+age = 2024 -int(birth_year)
+print(age)
+
+num1 = 20.1
+num2 = 30.9
+num3= num1 + num2
+print(int(num3))
+print(float(num3))
