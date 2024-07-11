dbb500c1deadc896c755b925812e8aa0f219a3f1
MonikaS5 <monika.bhagade@gmail.com>
2024-07-11 13:53:03 +0100

    float and str

---
+++ add.py
@@ -0,0 +1,12 @@
+num1 = input("First number :")
+num2 = input("Second number :")
+sum = float(num1) + float(num2)
+print("Addition :" + str(sum))
+sub = float(num1) - float(num2)
+print("Subtraction : " + str(sub))
+
+# another way
+num1 = float(input("First number :"))
+num2 = float(input("Second number :"))
+sum = num1 + num2
+print("Addition : " + str(sum))
