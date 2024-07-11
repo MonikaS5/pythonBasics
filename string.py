c012e289a35e6afe46a364b48af0da779096b5fc
MonikaS5 <monika.bhagade@gmail.com>
2024-07-11 14:20:17 +0100

    string methods

---
+++ string.py
@@ -0,0 +1,14 @@
+name = 'John Doe'
+print(name.upper())
+print(name.lower())
+print(name.find('h'))
+print(name.replace('oe', 'ey'))
+print(name.find('o'))
+print('Doe' in name)
+print('Dey' in name)
+print(name.capitalize())
+print(name.count('o'))
+
+myColor = ("Red", "Green", "Blue")
+x = "-".join(myColor)
+print(x)
