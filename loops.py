719014467691e4c50cd49620a54eef2b037d58d2
MonikaS5 <monika.bhagade@gmail.com>
2024-07-12 14:30:25 +0100

    range and for loop

--- loops.py
+++ loops.py
@@ -13,4 +13,16 @@ numbers = [1, 2, 3, 4, 5, 6]
 for items in numbers:
     print(items)
 
+mynum1 = range(5)
+print(mynum1)
+for number in mynum1:
+    print(number)
 
+mynum = range(5, 10, 2)
+print(mynum)
+for number in mynum:
+    print(number)
+
+print("another way")
+for number in range(8, 11):
+    print(number)
