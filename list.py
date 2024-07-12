80afbdc2d64607280ab1eda514e0bd34c52c2ee5
MonikaS5 <monika.bhagade@gmail.com>
2024-07-12 14:11:58 +0100

    string methods

---
+++ list.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 7)
+numbers.append(6)
+print(numbers)
+print(len(numbers))
+print(1 in numbers)
+
+
+names = ["John", "Bob", "Tom"]
+print(names[1])
+print(names)
+print(names[-1])
+names[0] = "Ron"
+print(names)
+
